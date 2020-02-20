5efe95717142132236ddf357a0f6c9e15e15f4af
erlingling <1150318255@qq.com>
2020-02-20 22:01:08 +0800

    none

---
+++ 4-opencv/王咏翔.py
@@ -0,0 +1,8 @@
+import cv2
+jpgpath = "C:/Users/hp/Desktop/picture.jpg"
+jpg = cv2.imread(jpgpath)
+cv2.imshow("Image",jpg)
+cv2.waitKey(0)
+jpggray = cv2.cvtColor(jpg,cv2.COLOR_BGR2GRAY)
+cv2.imshow("Image",jpggray)
+cv2.waitKey(0) 
